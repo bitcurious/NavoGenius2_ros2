0896c46eaa0158165f852d944bddf17284f6648b
bitcurious <umeshmane280@gmil.com>
2024-01-07 12:01:14 +0000

    slam

---
+++ navogenius2_description/launch/gazebo.launch.py
@@ -0,0 +1,91 @@
+from launch_ros.actions import Node
+from launch_ros.substitutions import FindPackageShare
+from launch import LaunchDescription
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import PathJoinSubstitution
+from launch.substitutions import Command, LaunchConfiguration
+import os
+import xacro
+from ament_index_python.packages import get_package_share_directory
+
+
+def generate_launch_description():
+    share_dir = get_package_share_directory('navogenius2_description')
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    xacro_file = os.path.join(share_dir, 'urdf', 'navogenius2.xacro')
+    robot_description_config = xacro.process_file(xacro_file)
+    robot_urdf = robot_description_config.toxml()
+
+    robot_state_publisher_node = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        name='robot_state_publisher',
+        parameters=[
+            {'use_sim_time': True},
+            {'robot_description': robot_urdf}
+        ]
+    )
+
+    joint_state_publisher_node = Node(
+        package='joint_state_publisher',
+        executable='joint_state_publisher',
+        name='joint_state_publisher',
+        parameters= [{'use_sim_time': True}]
+    )
+
+    gazebo_server = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzserver.launch.py'
+            ])
+        ]),
+        launch_arguments={
+            'pause': 'false'
+        }.items()
+    )
+
+    gazebo_client = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            PathJoinSubstitution([
+                FindPackageShare('gazebo_ros'),
+                'launch',
+                'gzclient.launch.py'
+            ])
+        ])
+    )
+
+    urdf_spawn_node = Node(
+        package='gazebo_ros',
+        executable='spawn_entity.py',
+        arguments=[
+            '-entity', 'navogenius2',
+            '-topic', 'robot_description'
+        ],
+        output='screen'
+    )
+
+    diff_drive_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["diff_cont"],
+    )
+
+    joint_broad_spawner = Node(
+        package="controller_manager",
+        executable="spawner.py",
+        arguments=["joint_broad"],
+    )
+
+    return LaunchDescription([
+        robot_state_publisher_node,
+        joint_state_publisher_node,
+        gazebo_server,
+        gazebo_client,
+        urdf_spawn_node,
+        diff_drive_spawner,
+        joint_broad_spawner
+
+    ])
